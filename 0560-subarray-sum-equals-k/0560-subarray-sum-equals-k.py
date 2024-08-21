8cfc0df8411995da34756f7831a914c686c3ef02
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-21 19:56:50 +0530

    Time: 218 ms (90.55%), Space: 19.3 MB (85.7%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,23 +1,22 @@
 class Solution:
-    def subarraySum(self, arr: List[int], k: int) -> int:
-        n = len(arr) # size of the given array.
-        mpp = defaultdict(int)
-        preSum = 0
-        cnt = 0
+	def subarraySum(self, nums: List[int], k: int) -> int:
 
-        mpp[0] = 1 # Setting 0 in the map.
-        for i in range(n):
-            # add current element to prefix Sum:
-            preSum += arr[i]
+		result = 0 
+		prefix_sum = 0
+		dictionary = {0 : 1}
 
-            # Calculate x-k:
-            remove = preSum - k
+		for num in nums:
+		
+			prefix_sum = prefix_sum + num
 
-            # Add the number of subarrays to be removed:
-            cnt += mpp[remove]
+			if prefix_sum - k in dictionary:
+			
+				result = result + dictionary[prefix_sum - k]
 
-            # Update the count of prefix sum
-            # in the map.
-            mpp[preSum] += 1
+			if prefix_sum not in dictionary:
+			
+				dictionary[prefix_sum] = 1
+			else:
+				dictionary[prefix_sum] = dictionary[prefix_sum] + 1
 
-        return cnt
+		return result
