2a621c2d8ee83ea193b1b20e9c8b56976545c881
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-21 20:00:48 +0530

    Time: 230 ms (49.83%), Space: 19.4 MB (55.1%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,20 +1,22 @@
 class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        result = 0  # This will store the total number of subarrays whose sum equals k.
-        prefix_sum = 0  # This variable keeps track of the cumulative sum of elements as we iterate through the list.
-        dictionary = {0: 1}  # A dictionary to store the frequency of prefix sums. We initialize it with 0:1 to handle the case where a subarray itself equals k.
+	def subarraySum(self, nums: List[int], k: int) -> int:
 
-        for num in nums:
-            prefix_sum += num  # Update the cumulative sum with the current element.
+		result = 0 
+		prefix_sum = 0
+		dictionary = {0 : 1}
 
-            # Check if there's a prefix sum that, when subtracted from the current prefix sum, equals k.
-            if prefix_sum - k in dictionary:
-                result += dictionary[prefix_sum - k]  # If found, increment the result by the frequency of that prefix sum.
+		for num in nums:
+		
+			prefix_sum = prefix_sum + num
 
-            # Update the dictionary to include the current prefix sum.
-            if prefix_sum not in dictionary:
-                dictionary[prefix_sum] = 1  # If the prefix sum is not in the dictionary, add it with a frequency of 1.
-            else:
-                dictionary[prefix_sum] += 1  # If the prefix sum is already in the dictionary, increment its frequency.
+			if prefix_sum - k in dictionary:
+			
+				result = result + dictionary[prefix_sum - k]
 
-        return result  # Return the total count of subarrays whose sum equals k.
+			if prefix_sum not in dictionary:
+			
+				dictionary[prefix_sum] = 1
+			else:
+				dictionary[prefix_sum] = dictionary[prefix_sum] + 1
+
+		return result
