4b64438348dac7c585b3b339779b2e6dcef3db07
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-23 18:59:59 +0530

    Time: 38 ms (85.34%), Space: 16.5 MB (33.7%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,24 +1,27 @@
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
+    def nextPermutation(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        idx = n-1
-        i = idx - 1
-        while i >= 0:
-            if nums[i] >= nums[i+1]:
-                i -= 1
-            else:
-                break
-        if i > -1:
-            for j in range(n-1, i, -1):
-                if nums[j] > nums[i]:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    break
-        i += 1
-        while i < idx:
-            nums[idx], nums[i] = nums[i], nums[idx]
+        i = len(nums) - 2
+        while i >= 0 and nums[i + 1] <= nums[i]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            self.swap(nums, i, j)
+        self.reverse(nums, i + 1)
+
+    def reverse(self, nums, start):
+        i, j = start, len(nums) - 1
+        while i < j:
+            self.swap(nums, i, j)
             i += 1
-            idx -= 1
-            
+            j -= 1
+
+    def swap(self, nums, i, j):
+        temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
