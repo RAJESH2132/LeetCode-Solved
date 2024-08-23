b095b9b279d98173880dbf937bbeb123e610c554
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-23 18:43:15 +0530

    Time: 45 ms (41.93%), Space: 16.3 MB (96.33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,27 +1,17 @@
 class Solution:
-    def nextPermutation(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
-        """
-        i = len(nums) - 2
-        while i >= 0 and nums[i + 1] <= nums[i]:
-            i -= 1
-        if i >= 0:
-            j = len(nums) - 1
-            while nums[j] <= nums[i]:
-                j -= 1
-            self.swap(nums, i, j)
-        self.reverse(nums, i + 1)
-
-    def reverse(self, nums, start):
-        i, j = start, len(nums) - 1
-        while i < j:
-            self.swap(nums, i, j)
-            i += 1
-            j -= 1
-
-    def swap(self, nums, i, j):
-        temp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = temp
+    def nextPermutation(self, nums: List[int]) -> None:
+        k = len(nums) - 2
+        while k >= 0 and nums[k] >= nums[k + 1]:
+            k -= 1
+        if k == -1:
+            nums.reverse()
+            return
+        l = len(nums) - 1
+        while nums[l] <= nums[k]:
+            l -= 1
+        nums[k], nums[l] = nums[l], nums[k]
+        left, right = k + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
