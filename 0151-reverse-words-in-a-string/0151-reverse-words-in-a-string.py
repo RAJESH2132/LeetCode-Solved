6e461984556dfeaad64702de7f4777fafa27781c
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-26 16:10:31 +0530

    Time: 35 ms (67.4%), Space: 16.7 MB (18.11%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,31 +1,9 @@
-class Solution(object):
-    def reverseWords(self, s):
-        length = len(s)
-        word_positions = []  # To store start and end indices of words
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
+        rev_words = words[::-1]
+
+        rev_string = ' '.join(rev_words)
+
+        return rev_string
         
-        i = 0
-        # Iterate through the string to identify word boundaries
-        while i < length:
-            # Skip leading spaces
-            while i < length and s[i] == ' ':
-                i += 1
-            if i == length:
-                break
-            
-            start = i  # Start of the word
-            
-            # Move to the end of the word
-            while i < length and s[i] != ' ':
-                i += 1
-            end = i - 1  # End of the word
-            
-            # Store the start and end indices of the word
-            word_positions.append((start, end))
-        
-        result = []
-        # Reverse the order of the words and build the result string
-        for start, end in reversed(word_positions):
-            word = s[start:end + 1]
-            result.append(word)
-        
-        return ' '.join(result)
