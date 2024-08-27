dbe93e421067821e30fbc428de356277ea9f08b2
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-27 18:06:25 +0530

    Time: 47 ms (67.43%), Space: 17 MB (46.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        str1 = sorted(s)
-        str2 = sorted(t)
-        return str1==str2
+        # 1. sort it and see if it's the same string!
+        # s_res = ''.join(sorted(s))
+        # t_res = ''.join(sorted(t))
+        # if s_res == t_res:
+        #     return True
+        # return False
+
+        # 2. iterate through s & t, build frequency map and see if it differs
+        freq={}
+        for i in s:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
+        for j in t:
+            # check if j is in freq. if not, FALSE
+            if j in freq:
+                freq[j]-=1
+            else:
+                return False
+        
+        for i in freq:
+            if freq[i] != 0:
+                return False
+        return True
