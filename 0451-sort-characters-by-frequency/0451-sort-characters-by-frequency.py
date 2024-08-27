e721ad8f15c27b994f9f31e1c7d1e68586143961
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-27 21:49:36 +0530

    Time: 36 ms (94.52%), Space: 17.7 MB (81.46%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -4,8 +4,7 @@ class Solution:
 
         # Step 2: Sort the characters by how often they appear, in descending order
         sorted_chars = sorted(freq.items(), key=lambda x: -x[1])
-    
+
         # Step 3: Build the result string by repeating each character based on its frequency
         result = ''.join([char * count for char, count in sorted_chars])
-    
         return result
