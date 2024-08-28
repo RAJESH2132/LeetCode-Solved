767db460b19cc07fa9eed3e92d912063f98fc20b
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-28 18:16:20 +0530

    Time: 41 ms (37.76%), Space: 16.6 MB (20.59%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,26 +1,33 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        ans = 0
-        sign = 1
+        #2:44
+
         i = 0
-        n = len(s)
 
-        # Skip leading whitespace
-        while i < n and s[i] == ' ':
-            i += 1
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
 
-        # Check for sign
-        if i < n and (s[i] == '-' or s[i] == '+'):
-            sign = -1 if s[i] == '-' else 1
-            i += 1
+        while i < len(s) and s[i] == ' ':
+            i+=1
 
-        # Convert the digits to an integer
-        while i < n and s[i].isdigit():
-            ans = ans * 10 + int(s[i])
-            if ans * sign <= -2**31:
-                return -2**31
-            if ans * sign >= 2**31 - 1:
-                return 2**31 - 1
-            i += 1
+        sign = 1
+        if i < len(s) and s[i] == '-':
+            sign = -1
+            i+=1
+        elif i < len(s) and s[i] == '+':
+            i+=1 
+        
+        while i < len(s) and s[i] == '0':
+            i+=1
+        
+        num = 0
+        while i < len(s) and s[i].isdigit():
+            num *= 10
+            num += int(s[i])
+            i+=1
+            if num > INT_MAX:
+                return INT_MAX if sign == 1 else INT_MIN
 
-        return ans * sign
+        return num * sign
+
+        
