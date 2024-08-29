71a0b77b0748569dd9a750e9fb8a105f2d25ee64
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-29 13:36:39 +0530

    Time: 39 ms (50.48%), Space: 16.6 MB (60.48%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,32 +1,30 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
+        INT_MAX=(2**31)-1
+        INT_MIN=-2**31
+
+        s=s.strip()
+        if not s:
+            return 0
         
-        i = 0
-
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
-
-        while i < len(s) and s[i] == ' ':
-            i+=1
-
-        sign = 1
-        if i < len(s) and s[i] == '-':
-            sign = -1
-            i+=1
-        elif i < len(s) and s[i] == '+':
-            i+=1 
+        sign=1
+        if s[0]=='-':
+            sign=-1
+            s=s[1:]
+        elif s[0]=='+':
+            s=s[1:]
         
-        while i < len(s) and s[i] == '0':
-            i+=1
-        
-        num = 0
-        while i < len(s) and s[i].isdigit():
-            num *= 10
-            num += int(s[i])
-            i+=1
-            if num > INT_MAX:
-                return INT_MAX if sign == 1 else INT_MIN
-
-        return num * sign
-
+        num=0
+        for char in s:
+            if char.isdigit():
+                num=num*10+int(char)
+            else:
+                break
+            
+        num*=sign
+        if num>INT_MAX:
+            return INT_MAX
+        elif num<INT_MIN:
+            return INT_MIN
+        return num
         
