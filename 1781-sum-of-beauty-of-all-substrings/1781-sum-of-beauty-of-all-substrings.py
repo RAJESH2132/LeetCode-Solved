a3678d43a04138fa24aa90c5a1d313b60be44721
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-29 16:46:07 +0530

    Time: 963 ms (92.51%), Space: 16.3 MB (93.22%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,11 +1,26 @@
 class Solution:
     def beautySum(self, s: str) -> int:
-        res = 0
-        for i in range(len(s)):
-            count = defaultdict(int)
-            for j in range(i,len(s)):
-                count[s[j]] += 1
-                minF = min(count.values())
-                maxF = max(count.values())
-                res += (maxF -minF)
-        return res
+        # use dictionary to store the frequencies of characters at each index and calulcate the diff b/w most frequent nd least frequent chars
+        ans = 0
+        n = len(s)
+        for i in range(n):
+            freq = {}
+            maxF = 0
+            for j in range(i,n):
+
+                if s[j] in freq:
+                    freq[s[j]] +=1
+                
+                else:
+                    freq[s[j]] = 1
+
+                maxF = max(maxF, freq[s[j]])
+
+                minF = min(freq.values())
+
+                ans += (maxF-minF)
+
+        return ans
+
+                
+        
