51e4f9299c9e3106d5251564891eedf1aad07631
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-09-01 14:52:39 +0530

    Time: 49 ms (77.97%), Space: 17 MB (94.59%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,23 +1,35 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
-        n = len(nums)
-        low,high = 0,n-1
+    def search(self, arr: List[int], target: int) -> bool:
+        n = len(arr)  # size of the array
+        low, high = 0, n - 1
+
         while low <= high:
-            mid = (low+high)//2
-            if nums[mid] == target:
+            mid = (low + high) // 2
+
+            # if mid points to the target
+            if arr[mid] == target:
                 return True
-            if nums[low] == nums[mid]:
+
+            # Edge case:
+            if arr[low] == arr[mid] and arr[mid] == arr[high]:
                 low += 1
-            elif nums[low] <= nums[mid]:
-                if nums[low] <= target <= nums[mid]:
-                    high = mid-1
+                high -= 1
+                continue
+
+            # if left part is sorted
+            if arr[low] <= arr[mid]:
+                if arr[low] <= target <= arr[mid]:
+                    # element exists
+                    high = mid - 1
                 else:
-                    low = mid+1
-            elif nums[mid] <= nums[high]:
-                if nums[mid]<= target <=nums[high]:
-                    low = mid+1
+                    # element does not exist
+                    low = mid + 1
+            else:  # if right part is sorted
+                if arr[mid] <= target <= arr[high]:
+                    # element exists
+                    low = mid + 1
                 else:
-                    high = mid-1
+                    # element does not exist
+                    high = mid - 1
+
         return False
-
-
