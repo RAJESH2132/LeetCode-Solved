53fa5aa5b279ef8fb057fbdf7ff9a98fa054d0a1
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-09-02 15:15:38 +0530

    Time: 144 ms (30.12%), Space: 26.8 MB (6.66%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,26 +1,21 @@
 class Solution:
-    def singleNonDuplicate(self, nums: List[int]) -> int:
-        n = len(nums)
-        low = 0
-        high = n-1
-
-        # edge cases
-        if n ==1:
-            return nums[0]
-        if nums[0]!=nums[1]:
-            return nums[0]
-        if nums[n-1]!=nums[n-2]:
-            return nums[n-1]
-
+    def singleNonDuplicate(self, a: List[int]) -> int:
+        n = len(a)
+        low = 1
+        high = n-2
+        if n == 1:
+            return a[0]
+        if a[0] != a[1]:
+            return a[0]
+        if a[n-1]!=a[n-2]:
+            return a[n-1]
+        
         while low <= high:
-            mid = low + (high-low)//2
-
-            if nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]:
-                return nums[mid]
-
-            if (mid%2==0 and nums[mid]==nums[mid+1]) or (mid%2==1 and nums[mid] == nums[mid-1]):
+            mid = (low+high)//2
+            if a[mid]!=a[mid-1] and a[mid]!=a[mid+1]:
+                return a[mid]
+            if ((mid%2==0 and a[mid]==a[mid+1]) or (mid%2==1 and a[mid]==a[mid-1])):
                 low = mid+1
             else:
                 high = mid-1
         return -1
-        
