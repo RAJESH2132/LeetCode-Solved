2183a4967dc55fc3d652179e994e557c8c2f2cf4
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-09-02 17:46:39 +0530

    Time: 51 ms (23.92%), Space: 16.7 MB (63.23%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,23 +1,32 @@
 class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        low = 0
-        high = n-1
+    def findPeakElement(self, arr: List[int]) -> int:
+        n = len(arr)  # Size of the array
 
+        # Edge cases:
         if n == 1:
             return 0
-        elif nums[0]>nums[1]:
+        if arr[0] > arr[1]:
             return 0
-        elif nums[n-1] > nums[n-2]:
-            return n-1
-        
-        while low <= high:
-            mid = low + (high-low)//2
+        if arr[n - 1] > arr[n - 2]:
+            return n - 1
 
-            if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
+        low = 1
+        high = n - 2
+        while low <= high:
+            mid = (low + high) // 2
+
+            # If arr[mid] is the peak:
+            if arr[mid - 1] < arr[mid] and arr[mid] > arr[mid + 1]:
                 return mid
-            if nums[mid] > nums[mid-1]:
-                low = mid+1
+
+            # If we are in the left:
+            if arr[mid] > arr[mid - 1]:
+                low = mid + 1
+
+            # If we are in the right:
+            # Or, arr[mid] is a common point:
             else:
-                high = mid-1
+                high = mid - 1
+
+        # Dummy return statement
         return -1
