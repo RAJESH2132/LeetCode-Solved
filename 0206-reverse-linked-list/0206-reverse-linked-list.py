ee7dbce6c7cebd975594a323143cf176f6ee7c6b
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-09-07 16:51:29 +0530

    Time: 39 ms (44.36%), Space: 17.6 MB (85.5%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,28 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Base case:
-        # If the linked list is empty or has only one node,
-        # return the head as it is already reversed.
-        if head is None or head.next is None:
-            return head
+        temp = head
+        prev = None
+        while temp is not None:
+            front = temp.next
+            temp.next = prev
+            prev = temp
+            temp = front 
 
-        # Recursive step:
-        # Reverse the linked list starting from the second node (head.next).
-        new_head = self.reverseList(head.next)
-
-        # Save a reference to the node following
-        # the current 'head' node.
-        front = head.next
-
-        # Make the 'front' node point to the current
-        # 'head' node in the reversed order.
-        front.next = head
-
-        # Break the link from the current 'head' node
-        # to the 'front' node to avoid cycles.
-        head.next = None
-
-        # Return the 'new_head,' which is the new
-        # head of the reversed linked list.
-        return new_head
+        return prev
