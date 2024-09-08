96466efce0430f0cc877568f055da0cb7b7cf232
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-09-08 15:17:40 +0530

    Time: 42 ms (68.15%), Space: 19 MB (61.26%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -5,24 +5,15 @@
 #         self.next = None
 
 class Solution:
-    def detectCycle(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return None
-        
-        # 步驟 1: 使用快慢指針找到相遇點
-        slow = fast = head
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow,fast=head,head
         while fast and fast.next:
-            slow = slow.next
+            slow= slow.next
             fast = fast.next.next
-            if slow == fast:
-                break
-        else:
-            return None  # 沒有環
-        
-        # 步驟 2: 找到環的起始點
-        slow = head
-        while slow != fast:
-            slow = slow.next
-            fast = fast.next
-        
-        return slow
+            if fast == slow :
+                slow=head
+                while slow!=fast:
+                    slow=slow.next
+                    fast = fast.next
+                return fast
+        return None     
