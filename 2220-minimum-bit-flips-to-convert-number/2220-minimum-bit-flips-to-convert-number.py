b06f0e471846c8d11defef0c4114aec1a66a8502
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-09-19 16:45:07 +0530

    Delete 2220-minimum-bit-flips-to-convert-number directory

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minBitFlips(self, start: int, goal: int) -> int:
-        ans = start ^ goal
-        count = 0
-        for i in range(31):
-            if (ans & (1<<i)):
-                count += 1
-        return count
