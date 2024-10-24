358a7d78f49db3aed451ed6e1c90f278389ec2fd
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-10-24 18:22:47 +0530

    Time: 4 ms (98.6%), Space: 16.6 MB (15.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,11 +1,23 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
+    def isPalindrome(self, n: int) -> bool:
+        # -----------------------Solution1---------------------------------
         # If the number is negative, it's not a palindrome
-        if x < 0:
+        if n < 0:
             return False
         
         # Convert the number to a string
-        str_x = str(x)
+        str_x = str(n)
         
         # Check if the string is the same as its reverse
         return str_x == str_x[::-1]
+
+        # --------------------------Solution2----------------------------------
+        # original = n
+        # revNum = 0
+        # while n > 0:
+        #     digit = n % 10
+        #     revNum = revNum * 10 + digit
+        #     n = n // 10
+        # if original != revNum:
+        #     return False
+        # return True
