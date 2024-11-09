f00f07c917139b0cba7d207d18386d5228eaf6a9
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-11-09 22:53:13 +0530

    Time: 7 ms (46.6%), Space: 17.8 MB (68.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -2,17 +2,14 @@ class Solution:
     def moveZeroes(self, a: List[int]) -> None:
         j = -1
         n = len(a)
-        # Place the pointer j
         for i in range(n):
             if a[i] == 0:
                 j = i
                 break
 
-        # No non-zero elements
         if j == -1:
             return a
 
-        # Move the pointers i and j and swap accordingly
         for i in range(j + 1, n):
             if a[i] != 0:
                 a[i], a[j] = a[j], a[i]
