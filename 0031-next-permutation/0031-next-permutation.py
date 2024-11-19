0bad9913b190681cc29540bfe8a3743cae8e82f7
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-11-19 15:24:55 +0530

    Time: 0 ms (100%), Space: 16.7 MB (36.24%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,27 +1,34 @@
 class Solution:
-    def nextPermutation(self, nums):
+    def nextPermutation(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        i = len(nums) - 2
-        while i >= 0 and nums[i + 1] <= nums[i]:
-            i -= 1
-        if i >= 0:
-            j = len(nums) - 1
-            while nums[j] <= nums[i]:
-                j -= 1
-            self.swap(nums, i, j)
-        self.reverse(nums, i + 1)
+        n = len(nums)
+        index = -1
+        for i in range(n-2,-1,-1):
+            if nums[i] < nums[i+1]:
+                index = i
+                break
+        if index == -1:
+            self.rotated(nums, 0, n)
+            return
+            
+        for i in range(n-1,-1,-1):
+            if nums[i]>nums[index]:
+                nums[i], nums[index] = nums[index], nums[i]
+                break
+        
+        self.rotated(nums,index+1,n)
+    
+    def rotated(self,arr,start,end):
+        left = start
+        right = end-1
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+        # return arr
 
-    def reverse(self, nums, start):
-        i, j = start, len(nums) - 1
-        while i < j:
-            self.swap(nums, i, j)
-            i += 1
-            j -= 1
-
-    def swap(self, nums, i, j):
-        temp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = temp
+       
+        
+        
