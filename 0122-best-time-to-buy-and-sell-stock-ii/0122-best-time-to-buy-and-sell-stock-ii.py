5a820a80901e7dd3a23a7fef37397f45601b5c3e
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-11-22 12:27:52 +0530

    Time: 0 ms (100%), Space: 17.6 MB (83.81%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,11 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxPrice = 0  # This variable will store the maximum profit.
-        start = prices[0]  # Start with the first day's price.
+        maxPrice = 0  
+
+        for i in range(1,len(prices)):
+            if prices[i]>prices[i-1]:
+                maxPrice += prices[i]-prices[i-1]
         
-        for num in prices:
-            if start < num:  # If the current price is higher than the starting price (previous day price),
-                maxPrice += num - start  # add the difference to maxPrice.
-            start = num  # Update start to the current day's price for the next iteration.
-        
-        return maxPrice  # Return the total accumulated profit.
+        return maxPrice
