188a85254b62958e36e46663647e12447a70db4b
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-12-14 17:46:03 +0530

    Time: 0 ms (100%), Space: 17.3 MB (13.41%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,32 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
         index = -1
+        n = len(nums)
         for i in range(n-2,-1,-1):
-            if nums[i] < nums[i+1]:
+            if nums[i]<nums[i+1]:
                 index = i
                 break
         if index == -1:
-            self.rotated(nums, 0, n)
+            nums.reverse()
             return
-            
-        for i in range(n-1,-1,-1):
-            if nums[i]>nums[index]:
+        for i in range(n-1,index,-1):
+            if nums[i] > nums[index]:
                 nums[i], nums[index] = nums[index], nums[i]
                 break
-        
-        self.rotated(nums,index+1,n)
-    
-    def rotated(self,arr,start,end):
-        left = start
-        right = end-1
-        while left < right:
-            arr[left], arr[right] = arr[right], arr[left]
-            left += 1
-            right -= 1
-        # return arr
+        mid = index + 1
+        nums[index+1:] = reversed(nums[index+1:])
 
-       
-        
-        
+
