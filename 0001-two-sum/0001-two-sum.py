e6c47c706a9d6ff9839a121a448acfd29bec1dd3
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-12-27 22:19:18 +0530

    Time: 0 ms (100%), Space: 19 MB (6.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_dict = {}  # Dictionary to store seen numbers and their indices
-        
-        for i, num in enumerate(nums):
-            complement = target - num
-            
-            # Check if the complement is in the dictionary
-            if complement in num_dict:
-                return [num_dict[complement], i]
-            
-            # If not, add the current number to the dictionary
-            num_dict[num] = i
-        
-        # If no solution is found, return an empty list or handle it as needed
-        return []
+        seen = {}
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in seen:
+                return [seen[comp],i]
+            seen[nums[i]] = i
+        return -1
