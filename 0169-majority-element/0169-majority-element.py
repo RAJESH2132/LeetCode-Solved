fa395e3fc0d7e698d31218feeed0bd8ba346247d
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2025-01-14 18:59:54 +0530

    Time: 5 ms (65.63%), Space: 19.4 MB (25.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,22 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
         count = 0
-        element = None
+        element = -1
+        n = len(nums)
         for i in range(n):
-            if count == 0:
+            if count == 0 :
                 count = 1
                 element = nums[i]
-            elif element == nums[i]:
+            elif nums[i] == element:
                 count += 1
             else:
                 count -= 1
-        return element
-        
-        #This check is not necessary because it is given that majority element always exists in the array.
-        # count1 = 0
-        # for i in range(n):
-        #     if nums[i] == element:
-        #         count1 += 1
-        # if count1 > (n/2):
-        #     return element
+        return element 
