6fb558361fabb69b505625bb5acfd8191ac5d7c8
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2025-02-18 17:15:24 +0530

    Time: 139 ms (60.41%), Space: 78.6 MB (27.49%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,50 +1,61 @@
 class Node:
-    def __init__(self, key:int, value:int):
+    def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.next = None
         self.prev = None
-
+        self.next = None
 
 class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {}
-        self.head = Node(0,0)
-        self.tail = Node(0,0)
+    def __init__(self, cap):
+        self.capacity = cap
+        self.cache = {}  # Dictionary to store key -> Node
+        self.head = Node(0, 0)  # Dummy head
+        self.tail = Node(0, 0)  # Dummy tail
         self.head.next = self.tail
         self.tail.prev = self.head
-        
-    def _remove(self,node:Node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-    
-    def _add(self, node:Node):
+
+    def _add(self, node: Node):
+        """Adds a node to the tail (most recently used position)."""
         prev, nxt = self.tail.prev, self.tail
         prev.next = nxt.prev = node
         node.prev, node.next = prev, nxt
 
-    def get(self, key: int) -> int:
+    def _remove(self, node: Node):
+        """Removes a node from the doubly linked list."""
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def get(self, key):
+        """Returns value if key exists, else -1."""
         if key not in self.cache:
             return -1
         node = self.cache[key]
-        self._remove(node)
+        self._remove(node)  # Move to most recently used
         self._add(node)
         return node.value
 
+    def put(self, key, value):
+        """Inserts or updates key-value pair. Removes LRU if full."""
+        if self.capacity == 0:
+            return  # If cache size is 0, do nothing
 
-    def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self._remove(self.cache[key])
-        node = Node(key,value)
-        self.cache[key] = node
+            node = self.cache[key]
+            node.value = value  # Update existing node's value
+            self._remove(node)
+        else:
+            node = Node(key, value)
+            self.cache[key] = node
+        
         self._add(node)
+
         if len(self.cache) > self.capacity:
             lru = self.head.next  # Least recently used node
             self._remove(lru)
             del self.cache[lru.key]
-        
+
+    
 
 
 # Your LRUCache object will be instantiated and called as such:
