44acdd45612ea4e95dc4d02d924489f2eb82387e
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2025-02-19 15:55:06 +0530

    Time: 0 ms (100%), Space: 17.9 MB (11.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,17 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        low = 0 
-        mid = 0
-        high = n-1
+        low, mid, high = 0, 0, len(nums)-1
         while mid <= high:
-            if  nums[mid] == 0:
+            if nums[mid] == 0:
                 nums[low], nums[mid] = nums[mid], nums[low]
                 low += 1
-                mid+= 1
-            elif nums[mid] == 1:
+                mid += 1
+
+            elif nums[mid]==1:
                 mid += 1
             else:
                 nums[high], nums[mid] = nums[mid], nums[high]
                 high -= 1
+
+        
